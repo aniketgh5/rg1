792c287895213e8024fc10ab5f7561b90dc63fdd
aniketgh5 <aniket.ghosh@live.com>
2025-11-12 20:00:58 +0530

    stg2_added

--- main.tf
+++ main.tf
@@ -6,4 +6,12 @@ resource "azurerm_resource_group" "rgblock" {
 }
 
 
+resource "azurerm_storage_account" "stgblock" {
+  for_each = var.stgblock
+  name = each.value.stgname
+  resource_group_name = each.value.rgname
+  location = each.value.location
+  account_tier = each.value.tier
+  account_replication_type = each.value.rep
+}
 
--- variable.tf
+++ variable.tf
@@ -2,6 +2,18 @@ variable "rgblock" {
   type = map(object({
     rgname = string
     location = string
-    tags = list(string)
+    tags = map(string)
   }))
+}
+
+
+variable "stgblock" {
+  type = map(object({
+    stgname = string
+    location = string
+    tier = string
+    rep = string
+    rgname = string
+  }))
+
 }
